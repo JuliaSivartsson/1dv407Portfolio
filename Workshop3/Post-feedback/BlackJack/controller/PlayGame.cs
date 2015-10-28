8afad25b026bd1e8cb2db690538f20e04c460d25
Julia Sivartsson <jsigc09@student.lnu.se>
2015-10-28 12:55:44 +0100

    post-feedback

---
+++ Workshop3/Post-feedback/BlackJack/controller/PlayGame.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlackJack.view;
+using BlackJack.model;
+
+namespace BlackJack.controller
+{
+    class PlayGame : model.CardObserver
+    {
+
+        public PlayGame(model.Game a_game, view.IView a_view)
+        {
+            m_game = a_game;
+            m_view = a_view;
+            m_game.DealNewCard(this);
+        }
+
+        private IView m_view;
+        private model.Game m_game;
+
+        public enum Action
+        {
+            Hit,
+            Stand,
+            NewGame,
+            Quit,
+            Undefined
+        }
+
+        public bool Play()
+        {
+            ShowGame();
+
+            if (m_game.IsGameOver())
+            {
+                m_view.DisplayGameOver(m_game.IsDealerWinner());
+            }
+
+            PlayGame.Action input = m_view.GetInput();
+
+            if (input == Action.NewGame)
+            {
+                m_game.NewGame();
+            }
+            else if (input == Action.Hit)
+            {
+                m_game.Hit();
+            }
+            else if (input == Action.Stand)
+            {
+                m_game.Stand();
+            }
+
+            return input != Action.Quit;
+        }
+
+        private void ShowGame()
+        {
+
+            m_view.DisplayWelcomeMessage();
+
+            m_view.DisplayDealerHand(m_game.GetDealerHand(), m_game.GetDealerScore());
+            m_view.DisplayPlayerHand(m_game.GetPlayerHand(), m_game.GetPlayerScore());
+        } 
+
+        public void CardDealt()
+        {
+            m_view.Pause(1000);
+            ShowGame();
+        }
+    }
+}
---
+++ Workshop3/Post-feedback/BlackJack/model/rules/InternationalNewGameStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class InternationalNewGameStrategy : NewGameAbstractStrategy
+    {
+
+        override public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
+        {
+            DealCard(a_deck, a_player);
+            DealCard(a_deck, a_dealer);
+            DealCard(a_deck, a_player);
+
+            return true;
+        }
+    }
+}
